65b2ca2aa3641b4c84e742c64a82c31eca380686
HenryBass <63601449+HenryBass@users.noreply.github.com>
2021-02-21 17:55:40 -0500

    Add files via upload

---
+++ Tweaks/Items/PlatinumSkinBuff.cs
@@ -0,0 +1,21 @@
+using Terraria;
+using Terraria.ModLoader;
+
+namespace Tweaks.Items
+{
+    public class PlatinumSkinBuff : ModBuff
+    {
+        public override void SetDefaults()
+        {
+            DisplayName.SetDefault("Platinum Skin");
+            Description.SetDefault("Grants +16 defense.");
+            Main.buffNoTimeDisplay[Type] = false;
+            Main.debuff[Type] = false;
+        }
+
+        public override void Update(Player player, ref int buffIndex)
+        {
+            player.statDefense += 16;
+        }
+    }
+}
